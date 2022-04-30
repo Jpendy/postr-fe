cee4d8994835fe03b47533f02b338284a8928bc5
Jpendy <Jpendy256@gmail.com>
2022-04-29 19:46:38 -0700

    replaced loading h3s with proper loading spinners

--- src/containers/userDetailPage/UserDetailPage.jsx
+++ src/containers/userDetailPage/UserDetailPage.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import PostList from '../../components/postList/PostList'
 import useUserPosts from '../../hooks/useUserPosts'
 import UserInfo from '../../components/userInfo/UserInfo'
+import LoadingSpinner from '../../components/loadingSpinner/LoadingSpinner'
 
 export default function UserDetailPage({ match }) {
 
     const { user, posts, loading, error } = useUserPosts(match.params.id)
-    console.log(user)
-    if (loading) return <h3>Loading...</h3>
+
+    if (loading) return <LoadingSpinner />
     return (
         <div style={{ textAlign: "center", paddingBottom: "200px" }} >
             <h2>Posts by {user.displayName || user.email}</h2>
